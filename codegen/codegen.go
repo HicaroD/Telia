e10581bbcfbbaabf45b40792659ca325dd94ed3a
HicaroD <hdanrlley1@gmail.com>
2024-03-29 12:02:36 -0300

    feat(codegen): simple code generation and script to compile everything

--- codegen/codegen.go
+++ codegen/codegen.go
@@ -11,11 +11,46 @@ import (
 	"tinygo.org/x/go-llvm"
 )
 
+type moduleCache struct {
+	functions map[string]*cachedFunction
+}
+
+type cachedFunction struct {
+	fn *llvm.Value
+	ty *llvm.Type
+}
+
+func newModuleCache() *moduleCache {
+	return &moduleCache{
+		functions: map[string]*cachedFunction{},
+	}
+}
+
+func (cache *moduleCache) InsertFunction(name string, fn *llvm.Value, ty *llvm.Type) {
+	// TODO(errors)
+	if _, ok := cache.functions[name]; ok {
+		log.Fatalf("FUNCTION %s ALREADY DECLARED", name)
+	}
+	function := cachedFunction{
+		fn: fn,
+		ty: ty,
+	}
+	cache.functions[name] = &function
+}
+
+func (cache *moduleCache) GetFunction(name string) *cachedFunction {
+	if fn, ok := cache.functions[name]; ok {
+		return fn
+	}
+	return nil
+}
+
 type codegen struct {
-	context  llvm.Context
-	module   llvm.Module
-	builder  llvm.Builder
-	astNodes []ast.AstNode
+	context     llvm.Context
+	module      llvm.Module
+	builder     llvm.Builder
+	astNodes    []ast.AstNode
+	moduleCache *moduleCache
 }
 
 func NewCodegen(astNodes []ast.AstNode) *codegen {
@@ -24,10 +59,11 @@ func NewCodegen(astNodes []ast.AstNode) *codegen {
 	builder := context.NewBuilder()
 
 	return &codegen{
-		context:  context,
-		module:   module,
-		builder:  builder,
-		astNodes: astNodes,
+		context:     context,
+		module:      module,
+		builder:     builder,
+		astNodes:    astNodes,
+		moduleCache: newModuleCache(),
 	}
 }
 
@@ -51,7 +87,7 @@ func (codegen *codegen) Generate() error {
 		}
 	}
 
-	filename := "llvm.ll"
+	filename := "telia.ll"
 	file, err := os.Create(filename)
 	if err != nil {
 		return err
@@ -70,12 +106,12 @@ func (codegen *codegen) generateFnDecl(function *ast.FunctionDecl) error {
 	returnType := codegen.getType(function.RetType)
 	paramsTypes := codegen.getFieldListTypes(function.Params)
 	functionType := llvm.FunctionType(returnType, paramsTypes, function.Params.IsVariadic)
-
 	functionValue := llvm.AddFunction(codegen.module, function.Name, functionType)
+
+	codegen.moduleCache.InsertFunction(function.Name, &functionValue, &functionType)
+
 	functionBody := codegen.context.AddBasicBlock(functionValue, "entry")
-
 	codegen.builder.SetInsertPointAtEnd(functionBody)
-
 	codegen.generateBlock(function.Block)
 	return nil
 }
@@ -85,10 +121,21 @@ func (codegen *codegen) generateBlock(block *ast.BlockStmt) {
 		switch block.Statements[i].(type) {
 		// TODO: build function call stmt
 		case ast.FunctionCallStmt:
-			fmt.Println("FUNCTION CALL STATEMENT")
-		// TODO: build return stmt
+			calledFn := block.Statements[i].(ast.FunctionCallStmt)
+			function := codegen.moduleCache.GetFunction(calledFn.Name)
+			// TODO(errors): function not found on cache
+			if function == nil {
+				log.Fatalf("FUNCTION %s not found on module", calledFn.Name)
+			}
+			args := codegen.getExprList(calledFn.Args)
+			call := codegen.builder.CreateCall(*function.ty, *function.fn, args, "call")
+			fmt.Printf("BUILDING CALL: %s\n", call)
 		case ast.ReturnStmt:
 			fmt.Println("RETURN STATEMENT")
+			ret := block.Statements[i].(ast.ReturnStmt)
+			returnValue := codegen.getExpr(ret.Value)
+			generatedRet := codegen.builder.CreateRet(returnValue)
+			fmt.Printf("BUILDING RETURN: %s\n", generatedRet)
 		default:
 			log.Fatalf("unimplemented block statement: %s", reflect.TypeOf(block.Statements[i]))
 		}
@@ -101,7 +148,8 @@ func (codegen *codegen) generateExternDecl(external *ast.ExternDecl) error {
 		returnType := codegen.getType(external.Prototypes[i].RetType)
 		paramsTypes := codegen.getFieldListTypes(external.Prototypes[i].Params)
 		functionType := llvm.FunctionType(returnType, paramsTypes, external.Prototypes[i].Params.IsVariadic)
-		llvm.AddFunction(codegen.module, external.Prototypes[i].Name, functionType)
+		functionValue := llvm.AddFunction(codegen.module, external.Prototypes[i].Name, functionType)
+		codegen.moduleCache.InsertFunction(external.Prototypes[i].Name, &functionValue, &functionType)
 	}
 	return nil
 }
@@ -140,10 +188,41 @@ func (codegen *codegen) getType(ty ast.ExprType) llvm.Type {
 	return codegen.context.VoidType()
 }
 
-func (codegen *codegen) getFieldListTypes(fieldList *ast.FieldList) []llvm.Type {
-	types := make([]llvm.Type, len(fieldList.Fields))
-	for i := range fieldList.Fields {
-		types[i] = codegen.getType(fieldList.Fields[i].Type)
+func (codegen *codegen) getFieldListTypes(fields *ast.FieldList) []llvm.Type {
+	types := make([]llvm.Type, len(fields.Fields))
+	for i := range fields.Fields {
+		types[i] = codegen.getType(fields.Fields[i].Type)
 	}
 	return types
+}
+
+func (codegen *codegen) getExprList(expressions []ast.Expr) []llvm.Value {
+	values := make([]llvm.Value, len(expressions))
+	for i := range expressions {
+		values[i] = codegen.getExpr(expressions[i])
+	}
+	return values
+}
+
+func (codegen *codegen) getExpr(expr ast.Expr) llvm.Value {
+	switch expr.(type) {
+	case ast.LiteralExpr:
+		literalExpr := expr.(ast.LiteralExpr)
+		switch literalExpr.Kind {
+		case kind.INTEGER_LITERAL:
+			integerLiteral := uint64(literalExpr.Value.(int))
+			return llvm.ConstInt(codegen.context.Int32Type(), integerLiteral, false)
+		case kind.STRING_LITERAL:
+			stringLiteral := literalExpr.Value.(string)
+			globalStrPtr := codegen.builder.CreateGlobalStringPtr(stringLiteral, ".str")
+			return globalStrPtr
+		default:
+			log.Fatalf("unimplemented literal expr: %s", expr)
+		}
+	default:
+		log.Fatalf("unimplemented expr: %s", expr)
+	}
+	// NOTE: this line should be unreachable
+	fmt.Println("REACHING AN UNREACHABLE LINE AT getExpr AT getExpr")
+	return llvm.Value{}
 }
