6ca370591f92424669d3cd0049c6e28b4a78d80c
HicaroD <hdanrlley1@gmail.com>
2024-04-02 15:53:48 -0300

    refactor(lexer): moving out tokens to the top of the file

--- lexer/token/kind/kind.go
+++ lexer/token/kind/kind.go
@@ -6,34 +6,6 @@ import (
 
 type TokenKind int
 
-var KEYWORDS map[string]TokenKind = map[string]TokenKind{
-	"fn":     FN,
-	"return": RETURN,
-	"extern": EXTERN,
-	"if":     IF,
-	"elif":   ELIF,
-	"else":   ELSE,
-
-	"true":  TRUE_BOOL_LITERAL,
-	"false": FALSE_BOOL_LITERAL,
-
-	"bool": BOOL_TYPE,
-	"i8":   I8_TYPE,
-	"i16":  I16_TYPE,
-	"i32":  I32_TYPE,
-	"i64":  I64_TYPE,
-	"i128": I128_TYPE,
-}
-
-var BASIC_TYPES map[TokenKind]bool = map[TokenKind]bool{
-	BOOL_TYPE: true,
-	I8_TYPE:   true,
-	I16_TYPE:  true,
-	I32_TYPE:  true,
-	I64_TYPE:  true,
-	I128_TYPE: true,
-}
-
 const (
 	// EOF
 	EOF TokenKind = iota
@@ -101,6 +73,34 @@ const (
 	// -
 	MINUS
 )
+
+var KEYWORDS map[string]TokenKind = map[string]TokenKind{
+	"fn":     FN,
+	"return": RETURN,
+	"extern": EXTERN,
+	"if":     IF,
+	"elif":   ELIF,
+	"else":   ELSE,
+
+	"true":  TRUE_BOOL_LITERAL,
+	"false": FALSE_BOOL_LITERAL,
+
+	"bool": BOOL_TYPE,
+	"i8":   I8_TYPE,
+	"i16":  I16_TYPE,
+	"i32":  I32_TYPE,
+	"i64":  I64_TYPE,
+	"i128": I128_TYPE,
+}
+
+var BASIC_TYPES map[TokenKind]bool = map[TokenKind]bool{
+	BOOL_TYPE: true,
+	I8_TYPE:   true,
+	I16_TYPE:  true,
+	I32_TYPE:  true,
+	I64_TYPE:  true,
+	I128_TYPE: true,
+}
 
 func (kind TokenKind) String() string {
 	switch kind {
