47955cb65230e8bbf56cf8033b440f10f3448a4b
HicaroD <hdanrlley1@gmail.com>
2024-04-10 10:43:59 -0300

    feat(lexer): checking error from tokenizer

--- main.go
+++ main.go
@@ -27,7 +27,10 @@ func main() {
 
 	reader := bufio.NewReader(file)
 	lex := lexer.New(filename, reader)
-	tokens := lex.Tokenize()
+	tokens, err := lex.Tokenize()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	parser := parser.New(tokens)
 	astNodes, err := parser.Parse()
