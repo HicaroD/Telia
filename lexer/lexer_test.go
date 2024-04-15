84672e29e2692af453fa4617031c7420eb97bc49
HicaroD <hdanrlley1@gmail.com>
2024-04-14 21:30:32 -0300

    refactor(lex): using `t.Run()` for testing
    Fixes #6

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -2,6 +2,7 @@ package lexer
 
 import (
 	"bufio"
+	"fmt"
 	"strings"
 	"testing"
 
@@ -14,71 +15,73 @@ type tokenKindTest struct {
 	kind   kind.TokenKind
 }
 
-var tokenKinds []*tokenKindTest = []*tokenKindTest{
-	// Keywords
-	{"fn", kind.FN},
-	{"return", kind.RETURN},
-	{"extern", kind.EXTERN},
-	{"if", kind.IF},
-	{"elif", kind.ELIF},
-	{"else", kind.ELSE},
-	{"not", kind.NOT},
-
-	// Types
-	{"bool", kind.BOOL_TYPE},
-	{"i8", kind.I8_TYPE},
-	{"i16", kind.I16_TYPE},
-	{"i32", kind.I32_TYPE},
-	{"i64", kind.I64_TYPE},
-	{"u8", kind.U8_TYPE},
-	{"u16", kind.U16_TYPE},
-	{"u32", kind.U32_TYPE},
-	{"u64", kind.U64_TYPE},
-
-	// Other tokens
-	{"(", kind.OPEN_PAREN},
-	{")", kind.CLOSE_PAREN},
-	{"{", kind.OPEN_CURLY},
-	{"}", kind.CLOSE_CURLY},
-	{",", kind.COMMA},
-	{";", kind.SEMICOLON},
-	{".", kind.DOT},
-	{"..", kind.DOT_DOT},
-	{"...", kind.DOT_DOT_DOT},
-	{"=", kind.EQUAL},
-	{":=", kind.COLON_EQUAL},
-	{"!=", kind.BANG_EQUAL},
-	{"==", kind.EQUAL_EQUAL},
-	{">", kind.GREATER},
-	{">=", kind.GREATER_EQ},
-	{"<", kind.LESS},
-	{"<=", kind.LESS_EQ},
-	{"+", kind.PLUS},
-	{"-", kind.MINUS},
-	{"*", kind.STAR},
-	{"/", kind.SLASH},
-}
-
 func TestTokenKinds(t *testing.T) {
-	testFilename := "test.tt"
+	filename := "test.tt"
 
-	for _, expectedToken := range tokenKinds {
-		reader := bufio.NewReader(strings.NewReader(expectedToken.lexeme))
-		lexer := New(testFilename, reader)
-		tokenResult, err := lexer.Tokenize()
-		if err != nil {
-			t.Errorf("TestTokenKind(%q): unexpected error '%v'", expectedToken.lexeme, err)
-		}
+	tests := []*tokenKindTest{
+		// Keywords
+		{"fn", kind.FN},
+		{"return", kind.RETURN},
+		{"extern", kind.EXTERN},
+		{"if", kind.IF},
+		{"elif", kind.ELIF},
+		{"else", kind.ELSE},
+		{"not", kind.NOT},
 
-		if len(tokenResult) != 2 {
-			t.Errorf("TestTokenKind(%q): expected len(tokenResult) == 2, but got %q", expectedToken.lexeme, len(tokenResult))
-		}
-		if tokenResult[1].Kind != kind.EOF {
-			t.Errorf("TestTokenKind(%q): expected last token to be EOF, but got %q", expectedToken.lexeme, tokenResult[1].Kind)
-		}
-		if tokenResult[0].Kind != expectedToken.kind {
-			t.Errorf("TestTokenKind(%q): expected token to be %q, but got %q", expectedToken.lexeme, expectedToken.kind, tokenResult[0].Kind)
-		}
+		// Types
+		{"bool", kind.BOOL_TYPE},
+		{"i8", kind.I8_TYPE},
+		{"i16", kind.I16_TYPE},
+		{"i32", kind.I32_TYPE},
+		{"i64", kind.I64_TYPE},
+		{"u8", kind.U8_TYPE},
+		{"u16", kind.U16_TYPE},
+		{"u32", kind.U32_TYPE},
+		{"u64", kind.U64_TYPE},
+
+		// Other tokens
+		{"(", kind.OPEN_PAREN},
+		{")", kind.CLOSE_PAREN},
+		{"{", kind.OPEN_CURLY},
+		{"}", kind.CLOSE_CURLY},
+		{",", kind.COMMA},
+		{";", kind.SEMICOLON},
+		{".", kind.DOT},
+		{"..", kind.DOT_DOT},
+		{"...", kind.DOT_DOT_DOT},
+		{"=", kind.EQUAL},
+		{":=", kind.COLON_EQUAL},
+		{"!=", kind.BANG_EQUAL},
+		{"==", kind.EQUAL_EQUAL},
+		{">", kind.GREATER},
+		{">=", kind.GREATER_EQ},
+		{"<", kind.LESS},
+		{"<=", kind.LESS_EQ},
+		{"+", kind.PLUS},
+		{"-", kind.MINUS},
+		{"*", kind.STAR},
+		{"/", kind.SLASH},
+	}
+
+	for _, test := range tests {
+		t.Run(fmt.Sprintf("TestTokenKind('%q')", test.lexeme), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(test.lexeme))
+			lexer := New(filename, reader)
+			tokenResult, err := lexer.Tokenize()
+			if err != nil {
+				t.Errorf("unexpected error '%v'", err)
+			}
+
+			if len(tokenResult) != 2 {
+				t.Errorf("expected len(tokenResult) == 2, but got %q", len(tokenResult))
+			}
+			if tokenResult[1].Kind != kind.EOF {
+				t.Errorf("expected last token to be EOF, but got %q", tokenResult[1].Kind)
+			}
+			if tokenResult[0].Kind != test.kind {
+				t.Errorf("expected token to be %q, but got %q", test.kind, tokenResult[0].Kind)
+			}
+		})
 	}
 }
 
@@ -87,51 +90,52 @@ type tokenPosTest struct {
 	positions []token.Position
 }
 
-var tokenPos []*tokenPosTest = []*tokenPosTest{
-	{";", []token.Position{
-		{Filename: "test.tt", Line: 1, Column: 1},
-		{Filename: "test.tt", Line: 1, Column: 2}},
-	},
-	{";\n;", []token.Position{
-		{Filename: "test.tt", Line: 1, Column: 1},
-		{Filename: "test.tt", Line: 2, Column: 1},
-		{Filename: "test.tt", Line: 2, Column: 2}},
-	},
-	{"fn\nhello world\n;", []token.Position{
-		{Filename: "test.tt", Line: 1, Column: 1},
-		{Filename: "test.tt", Line: 2, Column: 1},
-		{Filename: "test.tt", Line: 2, Column: 7},
-		{Filename: "test.tt", Line: 3, Column: 1},
-		{Filename: "test.tt", Line: 3, Column: 2}},
-	},
-}
-
 func TestTokenPos(t *testing.T) {
-	testFilename := "test.tt"
+	filename := "test.tt"
 
-	for _, expectedPos := range tokenPos {
-		reader := bufio.NewReader(strings.NewReader(expectedPos.input))
-		lexer := New(testFilename, reader)
-		tokenResult, err := lexer.Tokenize()
-		if err != nil {
-			t.Errorf("TestTokenPos(%q): unexpected error '%v'", expectedPos.input, err)
-		}
+	tests := []*tokenPosTest{
+		{";", []token.Position{
+			{Filename: "test.tt", Line: 1, Column: 1},
+			{Filename: "test.tt", Line: 1, Column: 2}},
+		},
+		{";\n;", []token.Position{
+			{Filename: "test.tt", Line: 1, Column: 1},
+			{Filename: "test.tt", Line: 2, Column: 1},
+			{Filename: "test.tt", Line: 2, Column: 2}},
+		},
+		{"fn\nhello world\n;", []token.Position{
+			{Filename: "test.tt", Line: 1, Column: 1},
+			{Filename: "test.tt", Line: 2, Column: 1},
+			{Filename: "test.tt", Line: 2, Column: 7},
+			{Filename: "test.tt", Line: 3, Column: 1},
+			{Filename: "test.tt", Line: 3, Column: 2}},
+		},
+	}
 
-		if len(tokenResult) == 1 && tokenResult[0].Kind == kind.EOF {
-			t.Errorf("TestTokenPos(%q): expected at least one token, but only got EOF", expectedPos.input)
-		}
-
-		if len(tokenResult) != len(expectedPos.positions) {
-			t.Errorf("TestTokenPos(%q): expected len(tokenResult) == len(expectedPos.positions), expected %d, but got %d", expectedPos.input, len(tokenResult), len(expectedPos.positions))
-		}
-
-		for i, expectedPos := range expectedPos.positions {
-			lexeme := tokenResult[i].Lexeme
-			actualPos := tokenResult[i].Position
-			if expectedPos != actualPos {
-				t.Errorf("TestTokenPos(%q): expected token position to be the same, expected %q, but got %q", lexeme, expectedPos, actualPos)
+	for _, test := range tests {
+		t.Run(fmt.Sprintf("TestTokenPos(%q)", test.input), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(test.input))
+			lexer := New(filename, reader)
+			tokenResult, err := lexer.Tokenize()
+			if err != nil {
+				t.Errorf("unexpected error '%v'", err)
 			}
-		}
+
+			if len(tokenResult) == 1 && tokenResult[0].Kind == kind.EOF {
+				t.Errorf("expected at least one token, but only got EOF")
+			}
+
+			if len(tokenResult) != len(test.positions) {
+				t.Errorf("expected len(tokenResult) == len(expectedPos.positions), expected %d, but got %d", len(tokenResult), len(test.positions))
+			}
+
+			for i, expectedPos := range test.positions {
+				actualPos := tokenResult[i].Position
+				if expectedPos != actualPos {
+					t.Errorf("expected token position to be the same, expected %q, but got %q", expectedPos, actualPos)
+				}
+			}
+		})
 	}
 }
 
@@ -140,58 +144,60 @@ type tokenIdentTest struct {
 	isId   bool
 }
 
-var tokenIdent []*tokenIdentTest = []*tokenIdentTest{
-	{"hello", true},
-	{"world", true},
-	{"foobar", true},
-	{"hello_world_", true},
-	{"foo६४", true},
-	{"a۰۱۸", true},
-	{"bar９８７６", true},
-	{"ŝ", true},
-	{"ŝfoo", true},
-	{"a123456789", true}, // NOTE: starts with "a"
-	{"123456789", false},
-	// TODO: add float here
-
-	{"true", false},
-	{"false", false},
-	{"fn", false},
-	{"return", false},
-	{"if", false},
-	{"elif", false},
-	{"else", false},
-
-	{"bool", false},
-	{"i8", false},
-	{"i16", false},
-	{"i32", false},
-	{"i64", false},
-	{"u8", false},
-	{"u16", false},
-	{"u32", false},
-	{"u64", false},
-}
-
 func TestIsIdentifier(t *testing.T) {
-	testFilename := "test.tt"
+	filename := "test.tt"
 
-	for _, expectedTokenIdent := range tokenIdent {
-		reader := bufio.NewReader(strings.NewReader(expectedTokenIdent.lexeme))
-		lexer := New(testFilename, reader)
-		tokenResult, err := lexer.Tokenize()
-		if err != nil {
-			t.Errorf("TestIsIdentifier(%q): unexpected error '%v'", expectedTokenIdent.lexeme, err)
-		}
-		if len(tokenResult) != 2 {
-			t.Errorf("TestIsIdentifier(%q): expected a single token, but got %d", expectedTokenIdent.lexeme, len(tokenResult))
-		}
-		if tokenResult[1].Kind != kind.EOF {
-			t.Errorf("TestIsIdentifier(%q): expected last token to be EOF, but got %q", expectedTokenIdent.lexeme, tokenResult[1].Kind)
-		}
-		if tokenResult[0].Kind != kind.ID && expectedTokenIdent.isId {
-			t.Errorf("TestIsIdentifier(%q): expecting to be an identifier, but got %q", expectedTokenIdent.lexeme, tokenResult[0].Kind)
-		}
+	tests := []*tokenIdentTest{
+		{"hello", true},
+		{"world", true},
+		{"foobar", true},
+		{"hello_world_", true},
+		{"foo६४", true},
+		{"a۰۱۸", true},
+		{"bar９８７６", true},
+		{"ŝ", true},
+		{"ŝfoo", true},
+		{"a123456789", true}, // NOTE: starts with "a"
+		{"123456789", false},
+		// TODO: add float here
+
+		{"true", false},
+		{"false", false},
+		{"fn", false},
+		{"return", false},
+		{"if", false},
+		{"elif", false},
+		{"else", false},
+
+		{"bool", false},
+		{"i8", false},
+		{"i16", false},
+		{"i32", false},
+		{"i64", false},
+		{"u8", false},
+		{"u16", false},
+		{"u32", false},
+		{"u64", false},
+	}
+
+	for _, test := range tests {
+		t.Run(fmt.Sprintf("TestIsIdentifier('%q')", test.lexeme), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(test.lexeme))
+			lexer := New(filename, reader)
+			tokenResult, err := lexer.Tokenize()
+			if err != nil {
+				t.Errorf("unexpected error '%v'", err)
+			}
+			if len(tokenResult) != 2 {
+				t.Errorf("expected a single token, but got %d", len(tokenResult))
+			}
+			if tokenResult[1].Kind != kind.EOF {
+				t.Errorf("expected last token to be EOF, but got %q", tokenResult[1].Kind)
+			}
+			if tokenResult[0].Kind != kind.ID && test.isId {
+				t.Errorf("expecting to be an identifier, but got %q", tokenResult[0].Kind)
+			}
+		})
 	}
 }
 
@@ -200,52 +206,44 @@ type tokenLiteralTest struct {
 	literalKind kind.TokenKind
 }
 
-var tokenLiterals []*tokenLiteralTest = []*tokenLiteralTest{
-	// TODO: add float here
-	{"1", kind.INTEGER_LITERAL},
-	{"2", kind.INTEGER_LITERAL},
-	{"3", kind.INTEGER_LITERAL},
-	{"4", kind.INTEGER_LITERAL},
-	{"5", kind.INTEGER_LITERAL},
-	{"6", kind.INTEGER_LITERAL},
-	{"7", kind.INTEGER_LITERAL},
-	{"8", kind.INTEGER_LITERAL},
-	{"9", kind.INTEGER_LITERAL},
-	// {"-1", kind.NEGATIVE_INTEGER_LITERAL},
-	// {"-2", kind.NEGATIVE_INTEGER_LITERAL},
-	// {"-3", kind.NEGATIVE_INTEGER_LITERAL},
-	// {"-4", kind.NEGATIVE_INTEGER_LITERAL},
-	// {"-5", kind.NEGATIVE_INTEGER_LITERAL},
-	// {"-6", kind.NEGATIVE_INTEGER_LITERAL},
-	// {"-7", kind.NEGATIVE_INTEGER_LITERAL},
-	// {"-8", kind.NEGATIVE_INTEGER_LITERAL},
-	// {"-9", kind.NEGATIVE_INTEGER_LITERAL},
-	{"123456789", kind.INTEGER_LITERAL},
-	// {"-123456789", kind.NEGATIVE_INTEGER_LITERAL},
-	{"\"Hello world\"", kind.STRING_LITERAL},
-	{"true", kind.TRUE_BOOL_LITERAL},
-	{"false", kind.FALSE_BOOL_LITERAL},
-}
-
 func TestIsLiteral(t *testing.T) {
-	testFilename := "test.tt"
-	// TODO: use t.Run(...)
-	for _, expectedTokenLiteral := range tokenLiterals {
-		reader := bufio.NewReader(strings.NewReader(expectedTokenLiteral.lexeme))
-		lexer := New(testFilename, reader)
-		tokenResult, err := lexer.Tokenize()
-		if err != nil {
-			t.Errorf("TestIsIdentifier(%q): unexpected error '%v'", expectedTokenLiteral.lexeme, err)
-		}
+	filename := "test.tt"
 
-		if len(tokenResult) != 2 {
-			t.Errorf("TestIsIdentifier(%q): expected a single token, but got %d", expectedTokenLiteral.lexeme, len(tokenResult))
-		}
-		if tokenResult[1].Kind != kind.EOF {
-			t.Errorf("TestIsIdentifier(%q): expected last token to be EOF, but got %q", expectedTokenLiteral.lexeme, tokenResult[1].Kind)
-		}
-		if tokenResult[0].Kind != expectedTokenLiteral.literalKind {
-			t.Errorf("TestIsIdentifier(%q): expected to be a %q, but got %q", expectedTokenLiteral.lexeme, expectedTokenLiteral.literalKind, tokenResult[0].Kind)
-		}
+	tests := []*tokenLiteralTest{
+		{"1", kind.INTEGER_LITERAL},
+		{"2", kind.INTEGER_LITERAL},
+		{"3", kind.INTEGER_LITERAL},
+		{"4", kind.INTEGER_LITERAL},
+		{"5", kind.INTEGER_LITERAL},
+		{"6", kind.INTEGER_LITERAL},
+		{"7", kind.INTEGER_LITERAL},
+		{"8", kind.INTEGER_LITERAL},
+		{"9", kind.INTEGER_LITERAL},
+		{"123456789", kind.INTEGER_LITERAL},
+		// TODO: add float here
+		{"\"Hello world\"", kind.STRING_LITERAL},
+		{"true", kind.TRUE_BOOL_LITERAL},
+		{"false", kind.FALSE_BOOL_LITERAL},
+	}
+
+	for _, test := range tests {
+		t.Run(fmt.Sprintf("TestIsLiteral('%q')", test.lexeme), func(t *testing.T) {
+			reader := bufio.NewReader(strings.NewReader(test.lexeme))
+			lexer := New(filename, reader)
+			tokenResult, err := lexer.Tokenize()
+			if err != nil {
+				t.Errorf("unexpected error '%v'", err)
+			}
+
+			if len(tokenResult) != 2 {
+				t.Errorf("expected a single token, but got %d", len(tokenResult))
+			}
+			if tokenResult[1].Kind != kind.EOF {
+				t.Errorf("expected last token to be EOF, but got %q", tokenResult[1].Kind)
+			}
+			if tokenResult[0].Kind != test.literalKind {
+				t.Errorf("expected to be a %q, but got %q", test.literalKind, tokenResult[0].Kind)
+			}
+		})
 	}
 }
